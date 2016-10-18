72472863d736dca8d301b7607026a09ddb4da961
Anthony Gore <anthony@magnum.technology>
2016-10-18 11:46:59 +0700

    Putting tooltip into component

--- src/index.js
+++ src/index.js
@@ -3,25 +3,34 @@
  */
 
 (function (Vue) {
-    let VueToolTipDirective = {
-        params: ['position', 'content'],
-        bind(el, binding, vnode){
-            let oOption = {
-                target: el,
-                position: vnode.data.attrs.position || "top left",
-                content: vnode.data.attrs.position || "",
-                constrainToWindow: false
-            };
-            new Tooltip(oOption);
-        }
+
+    let VueToolTipComponent = {
+      props: [ 'position', 'content' ],
+      render(h) {
+        return h(
+          'span',
+          this.$slots.default
+        );
+      },
+      mounted() {
+        let oOption = {
+          target: this.$el,
+          position: this.position || 'top left',
+          content: this.content || '',
+          constrainToWindow: false
+        };
+        this.tooltip = new Tooltip(oOption);
+      },
+      destroyed() {
+        this.tooltip.destroy();
+      }
     };
 
-
     /*
-     * Install Vue Directive if Vue is available
+     * Install Vue Component if Vue is available
      */
 
     if (typeof Vue !== "undefined") {
-        Vue.directive('tooltip', VueToolTipDirective);
+        Vue.component('tooltip', VueToolTipComponent);
     }
 })(window.Vue);
